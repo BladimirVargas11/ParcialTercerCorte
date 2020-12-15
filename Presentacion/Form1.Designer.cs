1c731c0037b74ee4a9a1c417c56d58e7be1fda91
bladimirjunior2000 <bladimirjunior2000@hotmail.com>
2020-12-15 14:08:11 -0500

    Agregar archivos de proyecto.

---
+++ Presentacion/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Presentacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BotonAbrir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextoArchivo = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // BotonAbrir
+            // 
+            this.BotonAbrir.Location = new System.Drawing.Point(401, 45);
+            this.BotonAbrir.Name = "BotonAbrir";
+            this.BotonAbrir.Size = new System.Drawing.Size(75, 23);
+            this.BotonAbrir.TabIndex = 0;
+            this.BotonAbrir.Text = "ABRIR";
+            this.BotonAbrir.UseVisualStyleBackColor = true;
+            this.BotonAbrir.Click += new System.EventHandler(this.BotonAbrir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RUTA DEL ARCHIVO:";
+            // 
+            // TextoArchivo
+            // 
+            this.TextoArchivo.Location = new System.Drawing.Point(157, 45);
+            this.TextoArchivo.Name = "TextoArchivo";
+            this.TextoArchivo.Size = new System.Drawing.Size(216, 20);
+            this.TextoArchivo.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Archivos TXT (*.txt)|*.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TextoArchivo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BotonAbrir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BotonAbrir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextoArchivo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Presentacion/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TextoArchivo.ReadOnly = true;
+        }
+
+        private void BotonAbrir_Click(object sender, EventArgs e)
+        {
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.FileName != null)
+            {
+                string file = openFileDialog1.FileName;
+                //string mensaje = ProductoSedeService.Guardar(file); // Crear la clase ProductoSedeService y el método guardar
+                //MessageBox.Show(mensaje);
+                TextoArchivo.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
